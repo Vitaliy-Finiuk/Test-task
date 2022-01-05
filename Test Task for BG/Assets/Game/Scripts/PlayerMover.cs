0469f8639e192ac334b2646dabf8dc34de22d398
Vitaliy-Finiuk <70633302+Vitaliy-Finiuk@users.noreply.github.com>
2022-01-05 16:51:59 +0200

    add Player Movement

---
+++ Test Task for BG/Assets/Game/Scripts/PlayerMover.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerMover : MonoBehaviour
+{
+    private Transform _target;
+
+    private NavMeshAgent _navMeshAgent;
+    private void Start() {
+        _navMeshAgent =  GetComponent<NavMeshAgent>();
+        _target = FindObjectOfType<GreenZone>().gameObject.transform;
+    }
+    private void Update()
+    {
+        Movement();
+    }
+
+    private void Movement(){
+        _navMeshAgent.destination = _target.position;
+    }
+}
